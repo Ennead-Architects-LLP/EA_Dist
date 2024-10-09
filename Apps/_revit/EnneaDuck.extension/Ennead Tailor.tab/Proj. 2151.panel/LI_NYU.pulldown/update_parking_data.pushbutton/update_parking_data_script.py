e3ef239bf01fac6ba4815afe968e4eff63e7f776
Sen Zhang <zsenarchitect@gmail.com>
2024-10-09 09:56:23 -0400

    Auto Commit 2024-10-09_09-56-23

--- Apps/_revit/EnneaDuck.extension/Ennead Tailor.tab/Proj. 2151.panel/LI_NYU.pulldown/update_parking_data.pushbutton/update_parking_data_script.py
+++ Apps/_revit/EnneaDuck.extension/Ennead Tailor.tab/Proj. 2151.panel/LI_NYU.pulldown/update_parking_data.pushbutton/update_parking_data_script.py
@@ -27,7 +27,7 @@ DOC_NAME_MAP = {
     "2151_A_EA_NYULI_Site": "Site",
     "2151_A_EA_NYULI_Parking East":"Garage East",
     "2151_A_EA_NYULI_Parking West":"Garage West",
-    "2151_A_EA_NYULI_CUP_EXT": "CUP Surface",
+    "2151_A_EA_NYULI_CUP_EXT": "CUP",
     "2151_A_EA_NYULI_HOSPITAL_EXT": "Hospital"
 }
 
