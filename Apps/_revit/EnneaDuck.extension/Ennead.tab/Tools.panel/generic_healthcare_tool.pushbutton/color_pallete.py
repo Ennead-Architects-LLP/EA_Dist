2bdcc8a2f154e47853b14d736647b8b8ce676941
Sen Zhang <zsenarchitect@gmail.com>
2024-12-23 11:10:00 -0500

    Auto Commit 2024-12-23_11-10-00

--- Apps/_revit/EnneaDuck.extension/Ennead.tab/Tools.panel/generic_healthcare_tool.pushbutton/color_pallete.py
+++ Apps/_revit/EnneaDuck.extension/Ennead.tab/Tools.panel/generic_healthcare_tool.pushbutton/color_pallete.py
@@ -1,5 +1,5 @@
 from EnneadTab import NOTIFICATION
-from EnneadTab.REVIT import REVIT_COLOR_SCHEME, REVIT_FOLDER
+from EnneadTab.REVIT import REVIT_COLOR_SCHEME, REVIT_FORMS
 from pyrevit import forms
 
 def update_color_pallete(doc):
@@ -12,12 +12,20 @@ def update_color_pallete(doc):
         NOTIFICATION.messenger(main_text="No excel file selected")
         return
 
-    naming_map = {"department_color_map":"Primary_Department Category",
-                  "program_color_map":"Primary_Department Program Type"}
-
-
-    is_remove_bad = REVIT_FORMS.dialogue(main_text="Do you want to remove the bad color?", button_name="Remove Bad Color")
 
     print ("color sceme name has been defined")
+    department_color_scheme_name = forms.SelectFromList.show(options=["Primary_Department Category", "Primary_Department Program Type"], multiselect=False, title="Select the department color scheme", button_name="Select")
+    program_color_scheme_name = forms.SelectFromList.show(options=["Primary_Department Program Type", "Primary_Department Program Type"], multiselect=False, title="Select the program color scheme", button_name="Select")     
+
+    naming_map = {"department_color_map":department_color_scheme_name,
+                  "program_color_map":program_color_scheme_name}
+
+    options = ["Remove Bad Color", "Keep Bad Color"]
+    select_option = REVIT_FORMS.dialogue(options = options, main_text="Do you want to remove the bad color?")
+    if select_option == options[0]:
+        is_remove_bad = True
+    else:
+        is_remove_bad = False
+    
 
     REVIT_COLOR_SCHEME.load_color_template(doc, naming_map, excel_path, is_remove_bad)
