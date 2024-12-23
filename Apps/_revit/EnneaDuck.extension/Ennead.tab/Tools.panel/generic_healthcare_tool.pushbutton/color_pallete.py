2ae6cf35d8b7aa00d3a5be75b334326c58383666
Sen Zhang <zsenarchitect@gmail.com>
2024-12-23 11:06:23 -0500

    Auto Commit 2024-12-23_11-06-22

--- Apps/_revit/EnneaDuck.extension/Ennead.tab/Tools.panel/generic_healthcare_tool.pushbutton/color_pallete.py
+++ Apps/_revit/EnneaDuck.extension/Ennead.tab/Tools.panel/generic_healthcare_tool.pushbutton/color_pallete.py
@@ -1,12 +1,22 @@
-
-from EnneadTab.REVIT import REVIT_COLOR_SCHEME
+from EnneadTab import NOTIFICATION
+from EnneadTab.REVIT import REVIT_COLOR_SCHEME, REVIT_FOLDER
+from pyrevit import forms
 
 def update_color_pallete(doc):
-    print ("update_color_pallete")
+
 
     print ("excel path has been defined")
+    NOTIFICATION.messenger(main_text="Select the excel file that contains the color pallete")
+    excel_path = forms.pick_file(title="Select the excel file", filter="Excel Files (*.xls)|*.xls")
+    if not excel_path:
+        NOTIFICATION.messenger(main_text="No excel file selected")
+        return
 
-    print ("area scheme for GFA and DGSF has been defined")
+    naming_map = {"department_color_map":"Primary_Department Category",
+                  "program_color_map":"Primary_Department Program Type"}
+
+
+    is_remove_bad = REVIT_FORMS.dialogue(main_text="Do you want to remove the bad color?", button_name="Remove Bad Color")
 
     print ("color sceme name has been defined")
 
