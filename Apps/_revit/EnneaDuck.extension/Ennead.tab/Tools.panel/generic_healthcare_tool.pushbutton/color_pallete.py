6c6ddd941e0e93d5c10127e0e3172a62a8227c71
Sen Zhang <zsenarchitect@gmail.com>
2024-12-23 11:15:23 -0500

    Auto Commit 2024-12-23_11-15-23

--- Apps/_revit/EnneaDuck.extension/Ennead.tab/Tools.panel/generic_healthcare_tool.pushbutton/color_pallete.py
+++ Apps/_revit/EnneaDuck.extension/Ennead.tab/Tools.panel/generic_healthcare_tool.pushbutton/color_pallete.py
@@ -5,17 +5,22 @@ from pyrevit import forms
 def update_color_pallete(doc):
 
 
-    print ("excel path has been defined")
     NOTIFICATION.messenger(main_text="Select the excel file that contains the color pallete")
-    excel_path = forms.pick_file(title="Select the excel file", filter="Excel Files (*.xls)|*.xls")
+    excel_path = forms.pick_excel_file(title="Select the excel file")
     if not excel_path:
         NOTIFICATION.messenger(main_text="No excel file selected")
         return
 
 
-    print ("color sceme name has been defined")
+
     department_color_scheme_name = REVIT_COLOR_SCHEME.pick_color_scheme(doc, title="Select the department color scheme", button_name="Select department color scheme")
+    if not department_color_scheme_name:
+        NOTIFICATION.messenger(main_text="No department color scheme selected")
+        return
     program_color_scheme_name = REVIT_COLOR_SCHEME.pick_color_scheme(doc, title="Select the program color scheme", button_name="Select program color scheme")     
+    if not program_color_scheme_name:
+        NOTIFICATION.messenger(main_text="No program color scheme selected")
+        return
 
     naming_map = {"department_color_map":department_color_scheme_name,
                   "program_color_map":program_color_scheme_name}
